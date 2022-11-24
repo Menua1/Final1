575b34d56cf424a8fc9c4c728861677570766973
Menua <menya2012avah@mail.ru>
2022-11-24 13:50:27 +0300

    Metod1

--- final/Program.cs
+++ final/Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array = new string[8] {"Hi", "my", "hello", "world", "bro","resiver","project","perfect"};
 string[] arr = new string[array.Length];
+void SecondArray(string[] array, string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arr[count] = array[i];
+        count++;
+        }
+    }
+}
